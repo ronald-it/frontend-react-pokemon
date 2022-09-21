4996e62da240d1c1d657345e6d185bfca0e5b436
ronald-it <ronald.cirgofer@hotmail.com>
2022-09-21 18:55:39 +0200

    Datafetch en design af voor één Pokemon kaart

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
+import Homepage from "./components/Homepage";
 
 function App() {
+  const [pokemonData, setPokemonData] = useState({});
+
   return (
-    <div>
-      Switch naar workbranch
-    </div>
+      <Homepage
+          pokemonData={pokemonData}
+          setPokemonData={setPokemonData}
+      />
   );
 }
 
